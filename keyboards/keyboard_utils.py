36090d0893c396ac27a10947e406d315237517fc
Anton Kononchenko <akononche@gmail.com>
2023-10-15 12:03:10 +0300

    Initial commit

---
+++ keyboards/keyboard_utils.py
@@ -0,0 +1,31 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from lexicon.lexicon import LEXICON
+
+
+# Функция для формирования инлайн-клавиатуры на лету
+def create_inline_kb(width: int,
+                     *args: str,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
---
+++ lexicon/lexicon.py
@@ -0,0 +1,4 @@
+LEXICON_COMMANDS_RU: dict[str, str] = {
+    '/help': 'Справка по работе бота',
+    '/start': 'Команда для запуска бота'
+}
